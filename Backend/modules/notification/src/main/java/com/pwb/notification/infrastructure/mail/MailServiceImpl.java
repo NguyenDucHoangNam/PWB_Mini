afd5cb692629843a9fe887dc4ea1fd5f7988ce49
nguyenduchoangnam <namnguyenduchoang@gmail.com>
2026-07-18 18:13:59 +0700

    feat(notification): update MailService profiles configuration

--- Backend/modules/notification/src/main/java/com/pwb/notification/infrastructure/mail/MailServiceImpl.java
+++ Backend/modules/notification/src/main/java/com/pwb/notification/infrastructure/mail/MailServiceImpl.java
@@ -4,6 +4,7 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -11,7 +12,8 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@Profile("prod")
+@Primary
+@Profile("!test")
 @RequiredArgsConstructor
 public class MailServiceImpl implements MailService {
 
--- Backend/modules/notification/src/main/java/com/pwb/notification/infrastructure/mail/NoOpMailService.java
+++ Backend/modules/notification/src/main/java/com/pwb/notification/infrastructure/mail/NoOpMailService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@Profile({"dev", "test", "local"})
+@Profile("test")
 public class NoOpMailService implements MailService {
 
     @Override
